d47b84b64a2a068b256b4c9703cdf5bab64e5406
Nicole Matthews <matthenm@miamioh.edu>
2019-03-06 18:01:35 -0500

    123

--- SPAnalytics/src/application/Main.java
+++ SPAnalytics/src/application/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
 			// Initializing the FXML loader
 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Admin_NetChart.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/SPAnalytics-Login.fxml"));
 
 			// Creating an instance of the controller class
 			Controller controller = new Controller();
